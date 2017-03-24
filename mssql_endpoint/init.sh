5b102a954be853f15f078554786089eedf4fff6f
Jeff Beorse <jbeorse@cs.washington.edu>
2017-03-24 14:20:45 -0700

    Fix init script

--- mssql_endpoint/init.sh
+++ mssql_endpoint/init.sh
@@ -1,48 +1,47 @@
 #!/bin/bash
 
 # Set path variables
-BASEPATH="/usr/local/tomcat/webapps"
-ROOTPATH="$BASEPATH/ROOT"
-WARPATH="$ROOTPATH.war"
-LIBPATH="$ROOTPATH/WEB-INF/lib"
-SETTINGSPATH="$LIBPATH/odk-settings-latest"
-JARPATH="$SETTINGSPATH.jar"
-JDBCPATH="$SETTINGSPATH/jdbc.properties"
+BASEPATH="/tmp"
+WARPATH="$BASEPATH/ODKAggregate.war"
+ROOTPATH="$BASEPATH/ODKAggregate"
+SETTINGSNAME="odk-settings-latest"
+SETTINGSPATH="$BASEPATH/$SETTINGSNAME"
+SETTINGSJARPATH="$ROOTPATH/WEB-INF/lib/$SETTINGSNAME.jar"
 SECURITYPATH="$SETTINGSPATH/security.properties"
+JDBCPATH="$SETTINGSPATH/jdbc.properties"
+RESULTPATH="/usr/local/tomcat/webapps/ROOT"
 
 # Sed replacement strings for properties
-DBUSERNAME_PROP="s/jdbc\.username=.*/jdbc\.username=${DBUSERNAME:="odk_user"}/g"
-DBPASSWORD_PROP="s/jdbc\.password=.*/jdbc\.password=$DBPASSWORD/g"
-DBSCHEMA_PROP="s/jdbc\.schema=.*/jdbc\.schema=${DBSCHEMA:="odk_prod"}/g"
-DBURL_PROP="s/jdbc\.url=.*/jdbc\.url=jdbc:sqlserver:\/\/$DBURL/g"
+## Server hostname
 SERVERURL_PROP="s/security\.server\.hostname=.*/security\.server\.hostname=$SERVERURL/g"
+## Server port
 SERVERPORT_PROP="s/security\.server\.port=.*/security\.server\.port=${SERVERPORT:="8080"}/g"
+## Secure server port
 SERVERSECUREPORT_PROP="s/security\.server\.securePort=.*/security\.server\.securePort=${SERVERSECUREPORT:="8443"}/g"
+## JDBC URL
+JDBCURL_PROP="s/jdbc\.url=.*/jdbc\.url=jdbc:sqlserver:\/\/$DBURL;database=$DBNAME;user=${DBUSERNAME:="odk_user"};password=$DBPASSWORD;encrypt=true;trustServerCertificate=${TRUSTSERVERCERT:="false"};hostNameInCertificate=${SERVERHOSTINCERT:="*.database.windows.net"};loginTimeout=30;authentication=ActiveDirectoryPassword/g"
+## Database Schema
+DBSCHEMA_PROP="s/jdbc\.schema=.*/jdbc\.schema=${DBSCHEMA:="odk_prod"}/g"
 
 
 # Unzip war and jar files
-rm -rf $ROOTPATH
 unzip $WARPATH -d $ROOTPATH
-unzip $JARPATH -d $SETTINGSPATH
-rm $WARPATH
-rm $JARPATH
+unzip $SETTINGSJARPATH -d $SETTINGSPATH
+rm $SETTINGSJARPATH
 
 # Update the settings with the configured values
-sed -i $DBUSERNAME_PROP $JDBCPATH
-sed -i $DBPASSWORD_PROP $JDBCPATH
-sed -i $DBSCHEMA_PROP $JDBCPATH
-sed -i $DBURL_PROP $JDBCPATH
 sed -i $SERVERURL_PROP $SECURITYPATH
 sed -i $SERVERPORT_PROP $SECURITYPATH
 sed -i $SERVERSECUREPORT_PROP $SECURITYPATH
+sed -i $JDBCURL_PROP $JDBCPATH
+sed -i $DBSCHEMA_PROP $JDBCPATH
 
 # Repackage the jar and war
 cd $SETTINGSPATH
-zip -r $JARPATH *
-cd $ROOTPATH
-rm -rf $SETTINGSPATH
-zip -r $WARPATH *
-rm -rf $ROOTPATH
+zip -r $SETTINGSJARPATH *
+mkdir $RESULTPATH
+cp -r $ROOTPATH/* $RESULTPATH
+
 
 # Start up tomcat
  /bin/bash /usr/local/tomcat/bin/catalina.sh run
